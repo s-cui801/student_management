872368181deed87a6a06ffa5b6c2bf23f99f3f29
Shiqi <cuisq0418@gmail.com>
2024-09-28 17:39:24 -0400

    Create student app; set up the student model

---
+++ students/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    date_of_birth = models.DateField()
+    enrollment_date = models.DateTimeField(auto_now_add=True)
+    grade = models.IntegerField()
