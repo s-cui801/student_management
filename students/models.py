e006a28b29fa89e09bf1560c4bd303397d489c09
Shiqi <cuisq0418@gmail.com>
2024-09-28 20:42:17 -0400

    Modify student model: adjust the type of enrollment_date to DateField

--- students/models.py
+++ students/models.py
@@ -6,5 +6,5 @@ class Student(models.Model):
     last_name = models.CharField(max_length=255)
     email = models.EmailField()
     date_of_birth = models.DateField()
-    enrollment_date = models.DateTimeField(auto_now_add=True)
+    enrollment_date = models.DateField()
     grade = models.IntegerField()
